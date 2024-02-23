56be13256e9c013ae4d7a5895a4e722897bd8971
Hima Bindu <bindu.python@gmail.com>
2024-02-23 18:27:58 +0530

    import data from frontend

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -4,7 +4,10 @@ import csv
 
 # from dataentry.models import Student
 
-from django.apps import apps  #to import all apps
+from django.apps import apps
+#to import all apps
+from django.db import DataError  
+
 
 # Proposed command - python manage.py importdata file_path model_name
 
@@ -35,8 +38,21 @@ class Command(BaseCommand):
     
         if not model:
             raise CommandError(f'Model "{model_name}" not found in any app!')
+       
+        #compare csv header with models field names
+        #get all the field names of the model that we found
+        model_fields=[field.name for field in model._meta.fields if field.name !='id']
+      
+       
         with open(file_path,'r') as file:
             reader = csv.DictReader(file)
+            csv_header=reader.fieldnames
+
+           #compare csv header with models field names
+            if csv_header != model_fields:
+                raise DataError(f"CSV file doesn't match' with the {model_name} table fields.")
+
+
             for row in reader:
                 model.objects.create(**row)
         self.stdout.write(self.style.SUCCESS('Data imported from csv successfully!'))   
